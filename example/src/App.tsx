3bda177717811fdeaf4bd779585019d8f791b914
hugos <schellinger@c-koya.tech>
2023-02-13 10:14:13 +0100

    V0.0.7

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function App(){
+  return (
+    <p>heyy</p>
+  )
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+
+export {Gantt} from "./components/gantt/gantt";
+export { ViewMode } from "./types/public-types";
+export type {
+  GanttProps,
+  ITask,
+  TaskType
+} from "./types/public-types";
